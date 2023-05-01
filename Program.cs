2a44f2e987f8e8fe0ba1e3dac16a5f4b62d14d83
rondastar <116147793+rondastar@users.noreply.github.com>
2023-05-01 10:41:25 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab_Objects_AllTheWayDown_Containment_Aggregation
+{
+    // Ronda Rutherford
+    // May 1, 2023
+    // Lab - Objects All The Way Down
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Lab_Objects-AllTheWayDown-Containment-Aggregation
+ Assignment using containment and aggregation for CSI 124 at Renton Tech
